481046cd183310af1c74a2c060931caba5f305fe
Gao, Rongjun <rongjun.gao@sap.com>
2022-11-17 17:49:51 +0800

    release version 0.3.0

--- ds4ml/__version__.py
+++ ds4ml/__version__.py
@@ -4,6 +4,6 @@
 __title__ = "ds4ml"
 __description__ = 'A Python library for data synthesis and evaluation'
 __url__ = 'https://github.com/SAP/data-synthesis-for-machine-learning'
-__version__ = '0.2.2'
+__version__ = '0.3.0'
 __maintainer__ = 'Yan Zhao'
 __maintainer_email__ = 'yan.zhao01@sap.com'
